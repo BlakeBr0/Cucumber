2d462c4e73a880b583fc9e42c8cf59dd343358a8
BlakeBr0 <luasmrks@gmail.com>
2017-08-11 00:09:17 -0500

    Add ItemDyeColorHandler for use by items that use EnumDyeColor

---
+++ 1.12/src/main/java/com/blakebr0/cucumber/lib/ItemDyeColorHandler.java
@@ -0,0 +1,17 @@
+package com.blakebr0.cucumber.lib;
+
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.ItemStack;
+
+/**
+ * A generic EnumDyeColor item color handler
+ * @author BlakeBr0
+ */
+public class ItemDyeColorHandler implements IItemColor {
+
+	@Override
+	public int getColorFromItemstack(ItemStack stack, int tintIndex){
+		return EnumDyeColor.byMetadata(stack.getMetadata()).getColorValue();
+	}
+}
