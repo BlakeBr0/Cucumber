4460c145c735c0072f28b3ef49bff116ca99f119
Unknown <luasmrks@gmail.com>
2018-01-14 17:36:54 -0600

    Add a drawTexturedModelRect to RenderHelper

--- 1.12/src/main/java/com/blakebr0/cucumber/helper/RenderHelper.java
+++ 1.12/src/main/java/com/blakebr0/cucumber/helper/RenderHelper.java
@@ -8,7 +8,10 @@ import com.blakebr0.cucumber.util.Utils;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
 public class RenderHelper {
 	
@@ -38,5 +41,26 @@ public class RenderHelper {
 	
 	public static void drawScaledWrappedText(FontRenderer font, String s, int x, int y, float scale, float width, int color, boolean shadow) {
 		drawScaledWrappedText(font, s, x, y, scale, width, -1, color, shadow);
+	}
+	
+	public static void drawTexturedModelRect(int x, int y, int textureX, int textureY, int width, int height) {
+		float f = 0.00390625F;
+		float f1 = 0.00390625F;
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder bufferbuilder = tessellator.getBuffer();
+		bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+		bufferbuilder.pos((double) (x + 0), (double) (y + height), (double) 0)
+					 .tex((double) ((float) (textureX + 0) * 0.00390625F), (double) ((float) (textureY + height) * 0.00390625F))
+					 .endVertex();
+		bufferbuilder.pos((double) (x + width), (double) (y + height), (double) 0)
+					 .tex((double) ((float) (textureX + width) * 0.00390625F), (double) ((float) (textureY + height) * 0.00390625F))
+					 .endVertex();
+		bufferbuilder.pos((double) (x + width), (double) (y + 0), (double) 0)
+					 .tex((double) ((float) (textureX + width) * 0.00390625F), (double) ((float) (textureY + 0) * 0.00390625F))
+					 .endVertex();
+		bufferbuilder.pos((double) (x + 0), (double) (y + 0), (double) 0)
+					 .tex((double) ((float) (textureX + 0) * 0.00390625F), (double) ((float) (textureY + 0) * 0.00390625F))
+					 .endVertex();
+		tessellator.draw();
 	}
 }
