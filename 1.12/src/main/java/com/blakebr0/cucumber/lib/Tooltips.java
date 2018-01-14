ba4abd4e70b9faf5dfa278aaedfaab9f9cd02d60
Unknown <luasmrks@gmail.com>
2018-01-14 17:33:14 -0600

    Add "hold shift for info" tooltip

---
+++ 1.12/src/main/java/com/blakebr0/cucumber/lib/Tooltips.java
@@ -0,0 +1,8 @@
+package com.blakebr0.cucumber.lib;
+
+import com.blakebr0.cucumber.util.Utils;
+
+public class Tooltips {
+
+	public static final String HOLD_SHIFT_FOR_INFO = Utils.localize("tooltip.cu.hold_shift_for_info");
+}
