ffea9af2b211068376a54ea59729715403bf4935
Unknown <luasmrks@gmail.com>
2018-07-01 15:21:11 -0500

    Added an exit button to the guide gui

--- src/main/java/com/blakebr0/cucumber/guide/GuiGuide.java
+++ src/main/java/com/blakebr0/cucumber/guide/GuiGuide.java
@@ -3,6 +3,7 @@ package com.blakebr0.cucumber.guide;
 import java.io.IOException;
 
 import com.blakebr0.cucumber.Cucumber;
+import com.blakebr0.cucumber.gui.button.GuiButtonExit;
 import com.blakebr0.cucumber.guide.pages.IEntryPage;
 import com.blakebr0.cucumber.helper.RenderHelper;
 import com.blakebr0.cucumber.helper.ResourceHelper;
@@ -36,6 +37,7 @@ public class GuiGuide extends GuiScreen {
 	public GuideEntry entry;
 	public int entryId;
 	public GuiButtonArrow prevEntry, nextEntry, prevPage, nextPage;
+	public GuiButton exit;
 	
 	public GuiGuide(ItemStack book, Guide guide) {
 		this.book = book;
@@ -64,6 +66,8 @@ public class GuiGuide extends GuiScreen {
 		this.prevPage = this.addButton(new GuiButtonArrow(10002, this.xStart + 196, this.yStart + 207, true));
 		this.nextPage = this.addButton(new GuiButtonArrow(10003, this.xStart + 390, this.yStart + 207, false));
 		
+		this.exit = this.addButton(new GuiButtonExit(10010, this.xStart + 412, this.yStart + 15));
+		
 		this.updateEntryButtons();
 		this.updatePageButtons();
 	}
@@ -78,7 +82,7 @@ public class GuiGuide extends GuiScreen {
 		RenderHelper.drawScaledCenteredWrappedTextXY(this.fontRenderer, Colors.BOLD + this.guide.getName(), this.xStart + 95, this.yStart + 22, 1.0F, 120, -1, 0, false);
 		
 		if (this.entry != null) {
-			RenderHelper.drawScaledCenteredWrappedTextY(this.fontRenderer, Colors.BOLD + this.entry.getTitle(), this.xStart + 227, this.yStart + 25, 1.0F, 190, -1, 0, false);
+			RenderHelper.drawScaledCenteredWrappedTextY(this.fontRenderer, Colors.BOLD + this.entry.getTitle(), this.xStart + 227, this.yStart + 25, 1.0F, 186, -1, 0, false);
 			RenderHelper.drawScaledItemIntoGui(this.itemRender, this.entry.getIconStack(), this.xStart + 197, this.yStart + 18, 1.54F);
 			if (this.entry.hasPages()) {
 				this.entry.getPage(this.entryPage).draw(this.mc, mouseX, mouseY, partialTicks, this.xStart + 200, this.yStart + 63, 220, 130);
@@ -89,6 +93,10 @@ public class GuiGuide extends GuiScreen {
 		RenderHelper.drawCenteredText(this.fontRenderer, Utils.localize("guide.cu.page_indicator", this.entryPage + 1, Math.max(this.maxEntryPage + 2, 1)), this.xStart + 308, this.yStart + 211, 0);
 
 		super.drawScreen(mouseX, mouseY, partialTicks);
+		
+		if (this.exit.isMouseOver()) {
+			this.drawHoveringText(this.exit.displayString, mouseX, mouseY);
+		}
 	}
 	
 	@Override
@@ -100,6 +108,8 @@ public class GuiGuide extends GuiScreen {
 			this.maxEntryPage = this.entry.getPageCount() - 2;
 			this.updateEntryButtons();
 			this.updatePageButtons();
+		} else if (button == this.exit) {
+			Minecraft.getMinecraft().displayGuiScreen(null);
 		} else if (button == this.prevEntry && this.topicsPage > 0) {
 			this.topicsPage -= 1;
 			this.updateEntryButtons();
