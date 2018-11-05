a8f25e50029cc857524524dcb19227855e70605a
Unknown <luasmrks@gmail.com>
2018-11-04 23:45:43 -0600

    Add default findAmmo method to ICustomBow

--- src/main/java/com/blakebr0/cucumber/iface/ICustomBow.java
+++ src/main/java/com/blakebr0/cucumber/iface/ICustomBow.java
@@ -1,10 +1,34 @@
 package com.blakebr0.cucumber.iface;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemArrow;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
 
 public interface ICustomBow {
 
 	default float getDrawSpeedMulti(ItemStack stack) {
 		return 1.0F;
+	}
+
+	default ItemStack findAmmo(EntityPlayer player) {
+		if (this.isArrow(player.getHeldItem(EnumHand.OFF_HAND))) {
+			return player.getHeldItem(EnumHand.OFF_HAND);
+		} else if (this.isArrow(player.getHeldItem(EnumHand.MAIN_HAND))) {
+			return player.getHeldItem(EnumHand.MAIN_HAND);
+		} else {
+			for (int i = 0; i < player.inventory.getSizeInventory(); i++) {
+				ItemStack stack = player.inventory.getStackInSlot(i);
+				if (this.isArrow(stack)) {
+					return stack;
+				}
+			}
+
+			return ItemStack.EMPTY;
+		}
+	}
+
+	default boolean isArrow(ItemStack stack) {
+		return stack.getItem() instanceof ItemArrow;
 	}
 }
