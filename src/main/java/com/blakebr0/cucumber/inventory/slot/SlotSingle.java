3ad4eb450550da40a5a0560bfb45048b663dc704
Unknown <luasmrks@gmail.com>
2018-11-04 22:32:06 -0600

    Added a slot for only stack sizes of 1

---
+++ src/main/java/com/blakebr0/cucumber/inventory/slot/SlotSingle.java
@@ -0,0 +1,16 @@
+package com.blakebr0.cucumber.inventory.slot;
+
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+
+public class SlotSingle extends Slot {
+
+	public SlotSingle(IInventory inventory, int index, int xPosition, int yPosition) {
+		super(inventory, index, xPosition, yPosition);
+	}
+
+	@Override
+	public int getSlotStackLimit() {
+		return 1;
+	}
+}
