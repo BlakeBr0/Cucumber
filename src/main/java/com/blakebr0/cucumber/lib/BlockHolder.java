28ca8488d7162d9714ff8516232b200962cf3edf
BlakeBr0 <luasmrks@gmail.com>
2019-03-01 18:21:32 -0600

    Add ItemHolder/BlockHolder Used for convenient creation of itemstacks for item instances and such
    Remove ItemPlaceholder

---
+++ src/main/java/com/blakebr0/cucumber/lib/BlockHolder.java
@@ -0,0 +1,25 @@
+package com.blakebr0.cucumber.lib;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+
+public class BlockHolder<T extends Block> {
+
+    private final T block;
+
+    public BlockHolder(T block) {
+        this.block = block;
+    }
+
+    public T getBlock() {
+        return this.block;
+    }
+
+    public ItemStack getItemStack() {
+        return new ItemStack(this.block);
+    }
+
+    public ItemStack getItemStack(int count) {
+        return new ItemStack(this.block, count);
+    }
+}
---
+++ src/main/java/com/blakebr0/cucumber/lib/ItemHolder.java
@@ -0,0 +1,25 @@
+package com.blakebr0.cucumber.lib;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class ItemHolder<T extends Item> {
+
+    private final T item;
+
+    public ItemHolder(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return this.item;
+    }
+
+    public ItemStack getItemStack() {
+        return new ItemStack(this.item);
+    }
+
+    public ItemStack getItemStack(int count) {
+        return new ItemStack(this.item, count);
+    }
+}
