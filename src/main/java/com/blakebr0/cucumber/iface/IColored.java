2e0f5163421ef720c679b72f368934c9307e0821
BlakeBr0 <luasmrks@gmail.com>
2019-03-28 17:02:22 -0500

    Refactor IColoredItem to IColored Add default color handlers for blocks/items that implement IColored

---
+++ src/main/java/com/blakebr0/cucumber/iface/IColored.java
@@ -0,0 +1,29 @@
+package com.blakebr0.cucumber.iface;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.color.IBlockColor;
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReaderBase;
+
+public interface IColored {
+
+	int color(int index);
+
+	class BlockColors implements IBlockColor {
+
+		@Override
+		public int getColor(IBlockState state, IWorldReaderBase world, BlockPos post, int index) {
+			return ((IColored) state.getBlock()).color(index);
+		}
+	}
+
+	class ItemColors implements IItemColor {
+
+		@Override
+		public int getColor(ItemStack stack, int index) {
+			return ((IColored) stack.getItem()).color(index);
+		}
+	}
+}
