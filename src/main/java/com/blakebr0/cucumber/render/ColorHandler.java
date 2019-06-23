b605dee5de248abf31904342e2c160fff32783c6
BlakeBr0 <luasmrks@gmail.com>
2019-06-23 18:42:25 -0500

    Update forge/mcp Change ColorHandler to use the mod event bus

--- src/main/java/com/blakebr0/cucumber/render/ColorHandler.java
+++ src/main/java/com/blakebr0/cucumber/render/ColorHandler.java
@@ -8,32 +8,37 @@ import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.util.IItemProvider;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ColorHandler {
-    private static final Map<IBlockColor, Block[]> BLOCKS_COLORS = Collections.synchronizedMap(new HashMap<>());
-    private static final Map<IItemColor, IItemProvider[]> ITEMS_COLORS = Collections.synchronizedMap(new HashMap<>());
+    private final Map<IBlockColor, Block[]> blocks = Collections.synchronizedMap(new HashMap<>());
+    private final Map<IItemColor, IItemProvider[]> items = Collections.synchronizedMap(new HashMap<>());
 
-    public static void registerBlocks(IBlockColor color, Block... blocks) {
-        BLOCKS_COLORS.put(color, blocks);
+    public void registerBlocks(IBlockColor color, Block... blocks) {
+        this.blocks.put(color, blocks);
     }
 
-    public static void registerItems(IItemColor color, IItemProvider... items) {
-        ITEMS_COLORS.put(color, items);
+    public void registerItems(IItemColor color, IItemProvider... items) {
+        this.items.put(color, items);
+    }
+
+    public ColorHandler() {
+        FMLJavaModLoadingContext.get().getModEventBus().register(this);
     }
 
     @SubscribeEvent
     public void onRegisterBlockColors(ColorHandlerEvent.Block event) {
         BlockColors colors = event.getBlockColors();
-        BLOCKS_COLORS.forEach(colors::register);
+        this.blocks.forEach(colors::register);
     }
 
     @SubscribeEvent
     public void onRegisterItemColors(ColorHandlerEvent.Item event) {
         ItemColors colors = event.getItemColors();
-        ITEMS_COLORS.forEach(colors::register);
+        this.items.forEach(colors::register);
     }
 }
