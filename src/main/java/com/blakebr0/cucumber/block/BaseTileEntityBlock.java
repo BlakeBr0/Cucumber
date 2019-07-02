ebd23f9bcb4bd45f034e5045a31b0fdecc778c5e
BlakeBr0 <luasmrks@gmail.com>
2019-07-02 18:59:23 -0500

    Add BaseTileEntityBlock for blocks with tile entities Start making deobf jars

---
+++ src/main/java/com/blakebr0/cucumber/block/BaseTileEntityBlock.java
@@ -0,0 +1,24 @@
+package com.blakebr0.cucumber.block;
+
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockReader;
+
+import java.util.function.Function;
+
+public class BaseTileEntityBlock extends BaseBlock implements ITileEntityProvider {
+    public BaseTileEntityBlock(Material material, Function<Properties, Properties> properties) {
+        super(material, properties);
+    }
+
+    public BaseTileEntityBlock(Material material, SoundType sound, float hardness, float resistance) {
+        super(material, sound, hardness, resistance);
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(IBlockReader world) {
+        return this.createTileEntity(null, world);
+    }
+}
