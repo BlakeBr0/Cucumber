ba1e62dfb6bf62c8249477eeb15da90f34dcca87
BlakeBr0 <luasmrks@gmail.com>
2019-07-20 17:07:28 -0500

    Add ability to 'disable' items Add BaseArmorItem

--- src/main/java/com/blakebr0/cucumber/item/BaseArmorItem.java
+++ src/main/java/com/blakebr0/cucumber/item/BaseArmorItem.java
@@ -1,23 +1,24 @@
 package com.blakebr0.cucumber.item;
 
 import com.blakebr0.cucumber.iface.IEnableable;
-import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 
 import java.util.function.Function;
 
-public class BaseBlockItem extends BlockItem {
-    public BaseBlockItem(Block block, Function<Properties, Properties> properties) {
-        super(block, properties.apply(new Properties()));
+public class BaseArmorItem extends ArmorItem {
+    public BaseArmorItem(IArmorMaterial material, EquipmentSlotType slot, Function<Properties, Properties> properties) {
+        super(material, slot, properties.apply(new Properties()));
     }
 
     @Override
     public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
-        if (this.getBlock() instanceof IEnableable) {
-            IEnableable enableable = (IEnableable) this.getBlock();
+        if (this instanceof IEnableable) {
+            IEnableable enableable = (IEnableable) this;
             if (enableable.isEnabled())
                 super.fillItemGroup(group, items);
         } else {
--- src/main/java/com/blakebr0/cucumber/item/BaseItem.java
+++ src/main/java/com/blakebr0/cucumber/item/BaseItem.java
@@ -1,11 +1,26 @@
 package com.blakebr0.cucumber.item;
 
+import com.blakebr0.cucumber.iface.IEnableable;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 
 import java.util.function.Function;
 
 public class BaseItem extends Item {
 	public BaseItem(Function<Properties, Properties> properties) {
 		super(properties.apply(new Properties()));
+	}
+
+	@Override
+	public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
+		if (this instanceof IEnableable) {
+			IEnableable enableable = (IEnableable) this;
+			if (enableable.isEnabled())
+				super.fillItemGroup(group, items);
+		} else {
+			super.fillItemGroup(group, items);
+		}
 	}
 }
