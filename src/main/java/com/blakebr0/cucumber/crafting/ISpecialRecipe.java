230139c84fa36b9d696eab3eb63122268ab1cd5f
BlakeBr0 <luasmrks@gmail.com>
2019-08-23 17:31:21 -0500

    Add ISpecialRecipe for recipes for custom crafters

---
+++ src/main/java/com/blakebr0/cucumber/crafting/ISpecialRecipe.java
@@ -0,0 +1,35 @@
+package com.blakebr0.cucumber.crafting;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.util.RecipeMatcher;
+import net.minecraftforge.items.IItemHandler;
+
+public interface ISpecialRecipe {
+    ItemStack getOutput();
+    NonNullList<Ingredient> getIngredients();
+    ResourceLocation getId();
+    ISpecialRecipeSerializer<?> getSerializer();
+
+    default boolean matches(IItemHandler inventory, int startIndex, int endIndex) {
+        NonNullList<ItemStack> inputs = NonNullList.create();
+        for (int i = startIndex; i < endIndex; i++) {
+            inputs.add(inventory.getStackInSlot(i));
+        }
+
+        return RecipeMatcher.findMatches(inputs, this.getIngredients()) != null;
+    }
+
+    default NonNullList<ItemStack> getRemainingItems(IItemHandler inventory) {
+        NonNullList<ItemStack> remaining = NonNullList.withSize(inventory.getSlots(), ItemStack.EMPTY);
+        for (int i = 0; i < remaining.size(); i++) {
+            ItemStack stack = inventory.getStackInSlot(i);
+            if (stack.hasContainerItem())
+                remaining.set(i, stack.getContainerItem());
+        }
+
+        return remaining;
+    }
+}
---
+++ src/main/java/com/blakebr0/cucumber/crafting/ISpecialRecipeSerializer.java
@@ -0,0 +1,11 @@
+package com.blakebr0.cucumber.crafting;
+
+import com.google.gson.JsonObject;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.ResourceLocation;
+
+public interface ISpecialRecipeSerializer<T extends ISpecialRecipe> {
+    T read(ResourceLocation recipeId, JsonObject json);
+    T read(ResourceLocation recipeId, PacketBuffer buffer);
+    void write(PacketBuffer buffer, T recipe);
+}
