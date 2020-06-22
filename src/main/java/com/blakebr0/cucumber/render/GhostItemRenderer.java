3d9a1fde0cca92a7c21c94f27eef26116fdfee4d
BlakeBr0 <luasmrks@gmail.com>
2020-06-22 02:15:38 -0500

    Add GhostItemRenderer.renderItemIntoGui to render a ghost item in a gui

--- src/main/java/com/blakebr0/cucumber/render/GhostItemRenderer.java
+++ src/main/java/com/blakebr0/cucumber/render/GhostItemRenderer.java
@@ -1,16 +1,26 @@
 package com.blakebr0.cucumber.render;
 
+import com.blakebr0.cucumber.client.ModRenderTypes;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.model.ModelResourceLocation;
 import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Direction;
 import org.lwjgl.opengl.GL14;
 
-public class GhostItemRenderer {
+import java.util.Random;
+
+public final class GhostItemRenderer {
     public static void renderItemModel(ItemStack stack, MatrixStack matrix, IRenderTypeBuffer buffer, float alpha) {
         if (!stack.isEmpty()) {
             Minecraft minecraft = Minecraft.getInstance();
@@ -36,5 +46,76 @@ public class GhostItemRenderer {
             minecraft.getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
             minecraft.getTextureManager().getTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
         }
+    }
+
+    public static void renderItemIntoGui(ItemStack stack, int x, int y, ItemRenderer itemRenderer) {
+        renderItemModelIntoGUI(stack, x, y, itemRenderer.getItemModelWithOverrides(stack, null, null), itemRenderer);
+    }
+
+    private static void renderModel(IBakedModel modelIn, ItemStack stack, int combinedLightIn, int combinedOverlayIn, MatrixStack matrixStackIn, IVertexBuilder bufferIn, ItemRenderer itemRenderer) {
+        Random random = new Random();
+
+        for (Direction direction : Direction.values()) {
+            random.setSeed(42L);
+            itemRenderer.renderQuads(matrixStackIn, bufferIn, modelIn.getQuads(null, direction, random), stack, combinedLightIn, combinedOverlayIn);
+        }
+
+        random.setSeed(42L);
+        itemRenderer.renderQuads(matrixStackIn, bufferIn, modelIn.getQuads(null, null, random), stack, combinedLightIn, combinedOverlayIn);
+    }
+
+    private static void renderItem(ItemStack itemStackIn, ItemCameraTransforms.TransformType transformTypeIn, boolean leftHand, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int combinedLightIn, int combinedOverlayIn, IBakedModel modelIn, ItemRenderer itemRenderer) {
+        if (!itemStackIn.isEmpty()) {
+            matrixStackIn.push();
+            boolean flag = transformTypeIn == ItemCameraTransforms.TransformType.GUI;
+            boolean flag1 = flag || transformTypeIn == ItemCameraTransforms.TransformType.GROUND || transformTypeIn == ItemCameraTransforms.TransformType.FIXED;
+            if (itemStackIn.getItem() == Items.TRIDENT && flag1) {
+                modelIn = itemRenderer.getItemModelMesher().getModelManager().getModel(new ModelResourceLocation("minecraft:trident#inventory"));
+            }
+
+            modelIn = net.minecraftforge.client.ForgeHooksClient.handleCameraTransforms(matrixStackIn, modelIn, transformTypeIn, leftHand);
+            matrixStackIn.translate(-0.5D, -0.5D, -0.5D);
+            if (!modelIn.isBuiltInRenderer() && (itemStackIn.getItem() != Items.TRIDENT || flag1)) {
+                IVertexBuilder ivertexbuilder = bufferIn.getBuffer(ModRenderTypes.GHOST);
+                renderModel(modelIn, itemStackIn, combinedLightIn, combinedOverlayIn, matrixStackIn, ivertexbuilder, itemRenderer);
+            } else {
+                itemStackIn.getItem().getItemStackTileEntityRenderer().render(itemStackIn, matrixStackIn, bufferIn, combinedLightIn, combinedOverlayIn);
+            }
+
+            matrixStackIn.pop();
+        }
+    }
+
+    private static void renderItemModelIntoGUI(ItemStack stack, int x, int y, IBakedModel bakedmodel, ItemRenderer itemRenderer) {
+        RenderSystem.pushMatrix();
+        Minecraft.getInstance().getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+        Minecraft.getInstance().getTextureManager().getTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE).setBlurMipmapDirect(false, false);
+        RenderSystem.enableRescaleNormal();
+        RenderSystem.enableAlphaTest();
+        RenderSystem.defaultAlphaFunc();
+        RenderSystem.enableBlend();
+        RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.translatef((float)x, (float)y, 100.0F + itemRenderer.zLevel);
+        RenderSystem.translatef(8.0F, 8.0F, 0.0F);
+        RenderSystem.scalef(1.0F, -1.0F, 1.0F);
+        RenderSystem.scalef(16.0F, 16.0F, 16.0F);
+        MatrixStack matrixstack = new MatrixStack();
+        IRenderTypeBuffer.Impl irendertypebuffer$impl = Minecraft.getInstance().getRenderTypeBuffers().getBufferSource();
+        boolean flag = !bakedmodel.func_230044_c_();
+        if (flag) {
+            net.minecraft.client.renderer.RenderHelper.setupGuiFlatDiffuseLighting();
+        }
+
+        renderItem(stack, ItemCameraTransforms.TransformType.GUI, false, matrixstack, irendertypebuffer$impl, 15728880, OverlayTexture.NO_OVERLAY, bakedmodel, itemRenderer);
+        irendertypebuffer$impl.finish();
+        RenderSystem.enableDepthTest();
+        if (flag) {
+            net.minecraft.client.renderer.RenderHelper.setupGui3DDiffuseLighting();
+        }
+
+        RenderSystem.disableAlphaTest();
+        RenderSystem.disableRescaleNormal();
+        RenderSystem.popMatrix();
     }
 }
