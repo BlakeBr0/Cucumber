54bc4867a14f5d213fc2ab1d1c5732619d34dbf4
BlakeBr0 <luasmrks@gmail.com>
2020-11-08 10:30:22 -0600

    Fix BaseOreBlock not requiring a pickaxe & added param for harvest level

--- src/main/java/com/blakebr0/cucumber/block/BaseOreBlock.java
+++ src/main/java/com/blakebr0/cucumber/block/BaseOreBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.IWorldReader;
+import net.minecraftforge.common.ToolType;
 
 import java.util.function.Function;
 
@@ -13,16 +14,24 @@ public class BaseOreBlock extends BaseBlock {
     private final int minExp;
     private final int maxExp;
 
+    @Deprecated // TODO: 1.17: remove
     public BaseOreBlock(Material material, Function<Properties, Properties> properties, int minExp, int maxExp) {
-        super(material, properties);
+        this(material, properties, 0, minExp, maxExp);
+    }
+
+    @Deprecated // TODO: 1.17: remove
+    public BaseOreBlock(Material material, SoundType sound, float hardness, float resistance, int minExp, int maxExp) {
+        this(material, sound, hardness, resistance, 0, minExp, maxExp);
+    }
+
+    public BaseOreBlock(Material material, Function<Properties, Properties> properties, int harvestLevel, int minExp, int maxExp) {
+        super(material, properties.compose(p -> p.harvestLevel(harvestLevel).harvestTool(ToolType.PICKAXE).setRequiresTool()));
         this.minExp = minExp;
         this.maxExp = maxExp;
     }
 
-    public BaseOreBlock(Material material, SoundType sound, float hardness, float resistance, int minExp, int maxExp) {
-        super(material, sound, hardness, resistance);
-        this.minExp = minExp;
-        this.maxExp = maxExp;
+    public BaseOreBlock(Material material, SoundType sound, float hardness, float resistance, int harvestLevel, int minExp, int maxExp) {
+        this(material, p -> p.sound(sound).hardnessAndResistance(hardness, resistance), harvestLevel, minExp, maxExp);
     }
 
     @Override
