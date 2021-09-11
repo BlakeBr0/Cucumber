c9b8bf7abb12f1c5e8089373db0fad33b4b5271f
BlakeBr0 <luasmrks@gmail.com>
2021-09-10 22:36:00 -0500

    Add ticker helper methods to BaseTileEntityBlock

--- src/main/java/com/blakebr0/cucumber/block/BaseTileEntityBlock.java
+++ src/main/java/com/blakebr0/cucumber/block/BaseTileEntityBlock.java
@@ -1,7 +1,12 @@
 package com.blakebr0.cucumber.block;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 
 import java.util.function.Function;
@@ -17,5 +22,25 @@ public abstract class BaseTileEntityBlock extends BaseBlock implements EntityBlo
 
     public BaseTileEntityBlock(Material material, SoundType sound, float hardness, float resistance, boolean tool) {
         super(material, sound, hardness, resistance, tool);
+    }
+
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return level.isClientSide()
+                ? this.getClientTicker(level, state, type)
+                : this.getServerTicker(level, state, type);
+    }
+
+    protected <T extends BlockEntity> BlockEntityTicker<T> getClientTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return null;
+    }
+
+    protected <T extends BlockEntity> BlockEntityTicker<T> getServerTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected static <E extends BlockEntity, A extends BlockEntity> BlockEntityTicker<A> createTicker(BlockEntityType<A> typeA, BlockEntityType<E> typeB, BlockEntityTicker<? super E> ticker) {
+        return typeA == typeB ? (BlockEntityTicker<A>) ticker : null;
     }
 }
