844acd9590b538ae5cf057eb001997f7a49093b4
BlakeBr0 <luasmrks@gmail.com>
2021-09-17 04:39:36 -0500

    Reimplement ghost render type

--- src/main/java/com/blakebr0/cucumber/client/ModRenderTypes.java
+++ src/main/java/com/blakebr0/cucumber/client/ModRenderTypes.java
@@ -1,29 +1,38 @@
 package com.blakebr0.cucumber.client;
 
-// TODO: 1.17, reimplement
-public final class ModRenderTypes {
-    //    public static final RenderType GHOST = RenderType.create(
-//            "cucumber:ghost",
-//            DefaultVertexFormat.BLOCK, VertexFormat.Mode.QUADS, 256, false, false,
-//            RenderType.CompositeState.builder()
-//                    .setTextureState(new RenderStateShard.TextureStateShard(TextureAtlas.LOCATION_BLOCKS, false, false))
-//                    .setAlphaState(new RenderStateShard.AlphaStateShard(0.5F) {
-//                        @Override
-//                        public void setupRenderState() {
-//                            RenderSystem.pushMatrix();
-//                            RenderSystem.color4f(1F, 1F, 1F, 1F);
-//                            GlStateManager._enableBlend();
-//                            GL14.glBlendColor(1.0F, 1.0F, 1.0F, 0.25F);
-//                            GlStateManager._blendFunc(GlStateManager.SourceFactor.CONSTANT_ALPHA.value, GlStateManager.DestFactor.ONE_MINUS_CONSTANT_ALPHA.value);
-//                        }
-//
-//                        @Override
-//                        public void clearRenderState() {
-//                            GL14.glBlendColor(1.0F, 1.0F, 1.0F, 1.0F);
-//                            GlStateManager._blendFunc(GlStateManager.SourceFactor.SRC_ALPHA.value, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA.value);
-//                            RenderSystem.disableBlend();
-//                            RenderSystem.popMatrix();
-//                        }
-//                    })
-//                    .createCompositeState(false));
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.VertexFormat;
+import net.minecraft.client.renderer.RenderStateShard;
+import net.minecraft.client.renderer.RenderType;
+import org.lwjgl.opengl.GL14;
+
+public final class ModRenderTypes extends RenderType {
+    public static final RenderType GHOST = RenderType.create(
+        "cucumber:ghost",
+        DefaultVertexFormat.BLOCK, VertexFormat.Mode.QUADS, 2097152, true, false,
+        RenderType.CompositeState.builder()
+                .setLightmapState(LIGHTMAP)
+                .setShaderState(RENDERTYPE_SOLID_SHADER)
+                .setTextureState(BLOCK_SHEET)
+                .setTransparencyState(new RenderStateShard.TransparencyStateShard("ghost_transparency",
+                        () -> {
+                            RenderSystem.enableBlend();
+                            RenderSystem.blendFunc(GlStateManager.SourceFactor.CONSTANT_ALPHA, GlStateManager.DestFactor.ONE_MINUS_CONSTANT_ALPHA);
+                            GL14.glBlendColor(1.0F, 1.0F, 1.0F, 0.25F);
+                        },
+                        () -> {
+                            GL14.glBlendColor(1.0F, 1.0F, 1.0F, 1.0F);
+                            RenderSystem.disableBlend();
+                            RenderSystem.defaultBlendFunc();
+                        }
+                ))
+                .createCompositeState(false)
+    );
+
+    // unused, just needed to extend RenderType for protected constants
+    private ModRenderTypes(String p_173178_, VertexFormat p_173179_, VertexFormat.Mode p_173180_, int p_173181_, boolean p_173182_, boolean p_173183_, Runnable p_173184_, Runnable p_173185_) {
+        super(p_173178_, p_173179_, p_173180_, p_173181_, p_173182_, p_173183_, p_173184_, p_173185_);
+    }
 }
