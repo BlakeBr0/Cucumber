18c9cfa0eeed0c0f3d32047983b2009fc524a384
BlakeBr0 <luasmrks@gmail.com>
2022-01-23 13:43:27 -0600

    Make BaseBowItem & BaseCrossbowItem implement ICustomBow Also add default getters for item render properties

--- src/main/java/com/blakebr0/cucumber/item/tool/BaseBowItem.java
+++ src/main/java/com/blakebr0/cucumber/item/tool/BaseBowItem.java
@@ -1,6 +1,8 @@
 package com.blakebr0.cucumber.item.tool;
 
+import com.blakebr0.cucumber.iface.ICustomBow;
 import com.blakebr0.cucumber.iface.IEnableable;
+import net.minecraft.client.renderer.item.ItemPropertyFunction;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -8,7 +10,7 @@ import net.minecraft.world.item.ItemStack;
 
 import java.util.function.Function;
 
-public class BaseBowItem extends BowItem {
+public class BaseBowItem extends BowItem implements ICustomBow {
     public BaseBowItem(Function<Properties, Properties> properties) {
         super(properties.apply(new Properties()));
     }
@@ -21,5 +23,26 @@ public class BaseBowItem extends BowItem {
         } else {
             super.fillItemCategory(group, items);
         }
+    }
+
+    @Override
+    public boolean hasFOVChange() {
+        return true;
+    }
+
+    public static ItemPropertyFunction getPullPropertyGetter() {
+        return (stack, level, entity, _unused) -> {
+            if (entity == null) {
+                return 0.0F;
+            } else {
+                return entity.getUseItem() != stack ? 0.0F : (float) (stack.getUseDuration() - entity.getUseItemRemainingTicks()) * ((ICustomBow) stack.getItem()).getDrawSpeedMulti(stack) / 20.0F;
+            }
+        };
+    }
+
+    public static ItemPropertyFunction getPullingPropertyGetter() {
+        return (stack, level, entity, _unused) -> {
+            return entity != null && entity.isUsingItem() && entity.getUseItem() == stack ? 1.0F : 0.0F;
+        };
     }
 }
--- src/main/java/com/blakebr0/cucumber/item/tool/BaseCrossbowItem.java
+++ src/main/java/com/blakebr0/cucumber/item/tool/BaseCrossbowItem.java
@@ -1,14 +1,17 @@
 package com.blakebr0.cucumber.item.tool;
 
+import com.blakebr0.cucumber.iface.ICustomBow;
 import com.blakebr0.cucumber.iface.IEnableable;
+import net.minecraft.client.renderer.item.ItemPropertyFunction;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 import java.util.function.Function;
 
-public class BaseCrossbowItem extends CrossbowItem {
+public class BaseCrossbowItem extends CrossbowItem implements ICustomBow {
     public BaseCrossbowItem(Function<Properties, Properties> properties) {
         super(properties.apply(new Properties()));
     }
@@ -21,5 +24,38 @@ public class BaseCrossbowItem extends CrossbowItem {
         } else {
             super.fillItemCategory(group, items);
         }
+    }
+
+    @Override
+    public boolean hasFOVChange() {
+        return false;
+    }
+
+    public static ItemPropertyFunction getPullPropertyGetter() {
+        return (stack, level, entity, _unused) -> {
+            if (entity == null) {
+                return 0.0F;
+            } else {
+                return BaseCrossbowItem.isCharged(stack) ? 0.0F : (float) (stack.getUseDuration() - entity.getUseItemRemainingTicks()) * ((ICustomBow) stack.getItem()).getDrawSpeedMulti(stack) / (float) BaseCrossbowItem.getChargeDuration(stack);
+            }
+        };
+    }
+
+    public static ItemPropertyFunction getPullingPropertyGetter() {
+        return (stack, level, entity, _unused) -> {
+            return entity != null && entity.isUsingItem() && entity.getUseItem() == stack && !CrossbowItem.isCharged(stack) ? 1.0F : 0.0F;
+        };
+    }
+
+    public static ItemPropertyFunction getChargedPropertyGetter() {
+        return (stack, level, entity, _unused) -> {
+            return entity != null && BaseCrossbowItem.isCharged(stack) ? 1.0F : 0.0F;
+        };
+    }
+
+    public static ItemPropertyFunction getFireworkPropertyGetter() {
+        return (stack, level, entity, _unused) -> {
+            return entity != null && BaseCrossbowItem.isCharged(stack) && BaseCrossbowItem.containsChargedProjectile(stack, Items.FIREWORK_ROCKET) ? 1.0F : 0.0F;
+        };
     }
 }
