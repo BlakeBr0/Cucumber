11a9e1d8493307f251a0b180222ca34006ec280c
BlakeBr0 <luasmrks@gmail.com>
2022-05-21 09:58:46 -0500

    Fix bows and crossbows not acctually accounting for custom draw speeds

--- src/main/java/com/blakebr0/cucumber/item/tool/BaseBowItem.java
+++ src/main/java/com/blakebr0/cucumber/item/tool/BaseBowItem.java
@@ -4,9 +4,20 @@ import com.blakebr0.cucumber.iface.ICustomBow;
 import com.blakebr0.cucumber.iface.IEnableable;
 import net.minecraft.client.renderer.item.ItemPropertyFunction;
 import net.minecraft.core.NonNullList;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
 
 import java.util.function.Function;
 
@@ -22,6 +33,71 @@ public class BaseBowItem extends BowItem implements ICustomBow {
                 super.fillItemCategory(group, items);
         } else {
             super.fillItemCategory(group, items);
+        }
+    }
+
+    @Override // copied from BowItem with the initial declaration of 'i' changed
+    public void releaseUsing(ItemStack stack, Level level, LivingEntity entity, int timeLeft) {
+        if (entity instanceof Player player) {
+            boolean flag = player.getAbilities().instabuild || EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, stack) > 0;
+            ItemStack itemstack = player.getProjectile(stack);
+
+            int i = (int) ((this.getUseDuration(stack) - timeLeft) * this.getDrawSpeedMulti(stack));
+            i = net.minecraftforge.event.ForgeEventFactory.onArrowLoose(stack, level, player, i, !itemstack.isEmpty() || flag);
+            if (i < 0) return;
+
+            if (!itemstack.isEmpty() || flag) {
+                if (itemstack.isEmpty()) {
+                    itemstack = new ItemStack(Items.ARROW);
+                }
+
+                float f = getPowerForTime(i);
+                if (!((double)f < 0.1D)) {
+                    boolean flag1 = player.getAbilities().instabuild || (itemstack.getItem() instanceof ArrowItem && ((ArrowItem)itemstack.getItem()).isInfinite(itemstack, stack, player));
+                    if (!level.isClientSide) {
+                        ArrowItem arrowitem = (ArrowItem)(itemstack.getItem() instanceof ArrowItem ? itemstack.getItem() : Items.ARROW);
+                        AbstractArrow abstractarrow = arrowitem.createArrow(level, itemstack, player);
+                        abstractarrow = customArrow(abstractarrow);
+                        abstractarrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, f * 3.0F, 1.0F);
+                        if (f == 1.0F) {
+                            abstractarrow.setCritArrow(true);
+                        }
+
+                        int j = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.POWER_ARROWS, stack);
+                        if (j > 0) {
+                            abstractarrow.setBaseDamage(abstractarrow.getBaseDamage() + (double)j * 0.5D + 0.5D);
+                        }
+
+                        int k = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.PUNCH_ARROWS, stack);
+                        if (k > 0) {
+                            abstractarrow.setKnockback(k);
+                        }
+
+                        if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FLAMING_ARROWS, stack) > 0) {
+                            abstractarrow.setSecondsOnFire(100);
+                        }
+
+                        stack.hurtAndBreak(1, player, (p_40665_) -> {
+                            p_40665_.broadcastBreakEvent(player.getUsedItemHand());
+                        });
+                        if (flag1 || player.getAbilities().instabuild && (itemstack.is(Items.SPECTRAL_ARROW) || itemstack.is(Items.TIPPED_ARROW))) {
+                            abstractarrow.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
+                        }
+
+                        level.addFreshEntity(abstractarrow);
+                    }
+
+                    level.playSound((Player)null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.PLAYERS, 1.0F, 1.0F / (level.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+                    if (!flag1 && !player.getAbilities().instabuild) {
+                        itemstack.shrink(1);
+                        if (itemstack.isEmpty()) {
+                            player.getInventory().removeItem(itemstack);
+                        }
+                    }
+
+                    player.awardStat(Stats.ITEM_USED.get(this));
+                }
+            }
         }
     }
 
--- src/main/java/com/blakebr0/cucumber/item/tool/BaseCrossbowItem.java
+++ src/main/java/com/blakebr0/cucumber/item/tool/BaseCrossbowItem.java
@@ -4,10 +4,20 @@ import com.blakebr0.cucumber.iface.ICustomBow;
 import com.blakebr0.cucumber.iface.IEnableable;
 import net.minecraft.client.renderer.item.ItemPropertyFunction;
 import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
 
 import java.util.function.Function;
 
@@ -26,9 +36,94 @@ public class BaseCrossbowItem extends CrossbowItem implements ICustomBow {
         }
     }
 
+    @Override // copied from CrossbowItem with the initial declaration of 'i' changed
+    public void releaseUsing(ItemStack stack, Level level, LivingEntity entity, int timeLeft) {
+        int i = (int) ((this.getUseDuration(stack) - timeLeft) * this.getDrawSpeedMulti(stack));
+        float f = getPowerForTime(i, stack);
+        if (f >= 1.0F && !isCharged(stack) && tryLoadProjectiles(entity, stack)) {
+            setCharged(stack, true);
+            SoundSource soundsource = entity instanceof Player ? SoundSource.PLAYERS : SoundSource.HOSTILE;
+            level.playSound((Player)null, entity.getX(), entity.getY(), entity.getZ(), SoundEvents.CROSSBOW_LOADING_END, soundsource, 1.0F, 1.0F / (level.getRandom().nextFloat() * 0.5F + 1.0F) + 0.2F);
+        }
+
+    }
+
     @Override
     public boolean hasFOVChange() {
         return false;
+    }
+
+    // copied from CrossbowItem as-is, may need to be checked in future updates
+    private static float getPowerForTime(int power, ItemStack stack) {
+        float f = (float) power / (float) getChargeDuration(stack);
+        if (f > 1.0F) {
+            f = 1.0F;
+        }
+
+        return f;
+    }
+
+    // copied from CrossbowItem as-is, may need to be checked in future updates
+    private static boolean tryLoadProjectiles(LivingEntity entity, ItemStack stack) {
+        int i = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.MULTISHOT, stack);
+        int j = i == 0 ? 1 : 3;
+        boolean flag = entity instanceof Player && ((Player)entity).getAbilities().instabuild;
+        ItemStack itemstack = entity.getProjectile(stack);
+        ItemStack itemstack1 = itemstack.copy();
+
+        for(int k = 0; k < j; ++k) {
+            if (k > 0) {
+                itemstack = itemstack1.copy();
+            }
+
+            if (itemstack.isEmpty() && flag) {
+                itemstack = new ItemStack(Items.ARROW);
+                itemstack1 = itemstack.copy();
+            }
+
+            if (!loadProjectile(entity, stack, itemstack, k > 0, flag)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // copied from CrossbowItem as-is, may need to be checked in future updates
+    private static boolean loadProjectile(LivingEntity entity, ItemStack bow, ItemStack arrow, boolean p_40866_, boolean p_40867_) {
+        if (arrow.isEmpty()) {
+            return false;
+        } else {
+            boolean flag = p_40867_ && arrow.getItem() instanceof ArrowItem;
+            ItemStack itemstack;
+            if (!flag && !p_40867_ && !p_40866_) {
+                itemstack = arrow.split(1);
+                if (arrow.isEmpty() && entity instanceof Player) {
+                    ((Player)entity).getInventory().removeItem(arrow);
+                }
+            } else {
+                itemstack = arrow.copy();
+            }
+
+            addChargedProjectile(bow, itemstack);
+            return true;
+        }
+    }
+
+    // copied from CrossbowItem as-is, may need to be checked in future updates
+    private static void addChargedProjectile(ItemStack p_40929_, ItemStack p_40930_) {
+        CompoundTag compoundtag = p_40929_.getOrCreateTag();
+        ListTag listtag;
+        if (compoundtag.contains("ChargedProjectiles", 9)) {
+            listtag = compoundtag.getList("ChargedProjectiles", 10);
+        } else {
+            listtag = new ListTag();
+        }
+
+        CompoundTag compoundtag1 = new CompoundTag();
+        p_40930_.save(compoundtag1);
+        listtag.add(compoundtag1);
+        compoundtag.put("ChargedProjectiles", listtag);
     }
 
     public static ItemPropertyFunction getPullPropertyGetter() {
