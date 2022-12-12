1ed72c5ba3e26a788138ef47e29b3b96341079f8
BlakeBr0 <luasmrks@gmail.com>
2022-12-11 20:38:11 -0600

    Re-add old widget tooltip handling "for now"

--- src/main/java/com/blakebr0/cucumber/client/screen/button/IconButton.java
+++ src/main/java/com/blakebr0/cucumber/client/screen/button/IconButton.java
@@ -7,32 +7,33 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
-// TODO: 1.19.3, validate how tooltips work
 public class IconButton extends Button {
 	private final ResourceLocation texture;
 	private final int textureX, textureY;
+	private final OnTooltip tooltip;
 
 	public IconButton(int x, int y, int width, int height, int textureX, int textureY, ResourceLocation texture, OnPress onPress) {
 		this(x, y, width, height, textureX, textureY, Component.literal(""), texture, onPress);
 	}
 
-	public IconButton(int x, int y, int width, int height, int textureX, int textureY, ResourceLocation texture, OnPress onPress, CreateNarration onTooltip) {
+	public IconButton(int x, int y, int width, int height, int textureX, int textureY, ResourceLocation texture, OnPress onPress, OnTooltip onTooltip) {
 		this(x, y, width, height, textureX, textureY, Component.literal(""), texture, onPress, onTooltip);
 	}
 
 	public IconButton(int x, int y, int width, int height, int textureX, int textureY, Component text, ResourceLocation texture, OnPress onPress) {
-		this(x, y, width, height, textureX, textureY, text, texture, onPress, DEFAULT_NARRATION);
+		this(x, y, width, height, textureX, textureY, text, texture, onPress, null);
 	}
 
-	public IconButton(int x, int y, int width, int height, int textureX, int textureY, Component text, ResourceLocation texture, OnPress onPress, CreateNarration onTooltip) {
-		super(x, y, width, height, text, onPress, onTooltip);
+	public IconButton(int x, int y, int width, int height, int textureX, int textureY, Component text, ResourceLocation texture, OnPress onPress, OnTooltip onTooltip) {
+		super(x, y, width, height, text, onPress, DEFAULT_NARRATION);
 		this.textureX = textureX;
 		this.textureY = textureY;
 		this.texture = texture;
+		this.tooltip = onTooltip;
 	}
 	
 	@Override
-	public void renderButton(PoseStack stack, int mouseX, int mouseY, float partialTicks) {
+	public void renderButton(PoseStack matrix, int mouseX, int mouseY, float partialTicks) {
 		RenderSystem.setShader(GameRenderer::getPositionTexShader);
 		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
 		RenderSystem.setShaderTexture(0, this.texture);
@@ -42,6 +43,15 @@ public class IconButton extends Button {
 
 		int i = this.getYImage(this.isHovered);
 
-		this.blit(stack, this.getX(), this.getY(), this.textureX, this.textureY + i * this.height, this.width, this.height);
+		this.blit(matrix, this.getX(), this.getY(), this.textureX, this.textureY + i * this.height, this.width, this.height);
+
+		if (this.tooltip != null && this.isHoveredOrFocused()) {
+			this.tooltip.render(this, matrix, mouseX, mouseY);
+		}
+	}
+
+	@FunctionalInterface
+	public interface OnTooltip {
+		void render(Button button, PoseStack matrix, int mouseX, int mouseY);
 	}
 }
