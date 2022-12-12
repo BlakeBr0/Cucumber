fd4947c10f239a276762144f9e9144dc18a7f56d
BlakeBr0 <luasmrks@gmail.com>
2022-12-11 20:38:39 -0600

    Make BaseContainerMenu not abstract

--- src/main/java/com/blakebr0/cucumber/container/BaseContainerMenu.java
+++ src/main/java/com/blakebr0/cucumber/container/BaseContainerMenu.java
@@ -4,13 +4,19 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.ItemStack;
 
-public abstract class BaseContainerMenu extends AbstractContainerMenu {
+public class BaseContainerMenu extends AbstractContainerMenu {
     private final BlockPos pos;
 
     protected BaseContainerMenu(MenuType<?> menu, int id, BlockPos pos) {
         super(menu, id);
         this.pos = pos;
+    }
+
+    @Override
+    public ItemStack quickMoveStack(Player player, int slot) {
+        return ItemStack.EMPTY;
     }
 
     @Override
