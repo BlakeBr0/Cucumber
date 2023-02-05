fda74e240334c28a63b033dfb6945c92d89737ee
BlakeBr0 <luasmrks@gmail.com>
2023-02-05 02:38:19 -0600

    Update almost unified integration

--- src/main/java/com/blakebr0/cucumber/compat/almostunified/AlmostUnifiedAdapter.java
+++ src/main/java/com/blakebr0/cucumber/compat/almostunified/AlmostUnifiedAdapter.java
@@ -1,11 +1,11 @@
 package com.blakebr0.cucumber.compat.almostunified;
 
 import com.almostreliable.unified.api.AlmostUnifiedLookup;
-import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class AlmostUnifiedAdapter {
     public static boolean isLoaded() {
@@ -14,7 +14,7 @@ public class AlmostUnifiedAdapter {
 
     public static Item getPreferredItemForTag(String tagId) {
         if (isLoaded()) {
-            var tagKey = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(tagId));
+            var tagKey = TagKey.create(ForgeRegistries.Keys.ITEMS, new ResourceLocation(tagId));
             return Adapter.getPreferredItemForTag(tagKey);
         }
 
