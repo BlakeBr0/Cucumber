590c995425c082e53e481dd5ec7352647ae7d2a8
BlakeBr0 <luasmrks@gmail.com>
2023-03-15 11:10:44 -0500

    Delete deprecated BlockHelper functions

--- src/main/java/com/blakebr0/cucumber/helper/BlockHelper.java
+++ src/main/java/com/blakebr0/cucumber/helper/BlockHelper.java
@@ -2,11 +2,8 @@ package com.blakebr0.cucumber.helper;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.monster.piglin.PiglinAi;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ClipContext;
@@ -16,11 +13,20 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.ForgeMod;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.level.BlockEvent;
 
 public final class BlockHelper {
-	private static BlockHitResult rayTraceBlocks(Level level, Player player, double reach, ClipContext.Fluid fluidMode) {
+    public static BlockHitResult rayTraceBlocks(Level level, Player player) {
+        return rayTraceBlocks(level, player, ClipContext.Fluid.NONE);
+    }
+
+    public static BlockHitResult rayTraceBlocks(Level level, Player player, ClipContext.Fluid fluidMode) {
+        var attribute = player.getAttribute(ForgeMod.REACH_DISTANCE.get());
+        var reach = attribute != null ? attribute.getValue() : 4.5D;
+
+        return rayTraceBlocks(level, player, reach, fluidMode);
+    }
+
+    private static BlockHitResult rayTraceBlocks(Level level, Player player, double reach, ClipContext.Fluid fluidMode) {
         var pitch = player.getXRot();
         var yaw = player.getYRot();
         var eyePos = player.getEyePosition(1.0F);
@@ -34,66 +40,6 @@ public final class BlockHelper {
         var vec3d1 = eyePos.add((double) f6 * reach, (double) f5 * reach, (double) f7 * reach);
 
         return level.clip(new ClipContext(eyePos, vec3d1, ClipContext.Block.OUTLINE, fluidMode, player));
-	}
-
-	public static BlockHitResult rayTraceBlocks(Level level, Player player) {
-		return rayTraceBlocks(level, player, ClipContext.Fluid.NONE);
-	}
-
-	public static BlockHitResult rayTraceBlocks(Level level, Player player, ClipContext.Fluid fluidMode) {
-        var attribute = player.getAttribute(ForgeMod.REACH_DISTANCE.get());
-        var reach = attribute != null ? attribute.getValue() : 5.0D;
-
-		return rayTraceBlocks(level, player, reach, fluidMode);
-	}
-
-    @Deprecated
-	public static boolean breakBlocksAOE(ItemStack stack, Level level, Player player, BlockPos pos) {
-	    return breakBlocksAOE(stack, level, player, pos, true);
-    }
-
-    @Deprecated
-    public static boolean breakBlocksAOE(ItemStack stack, Level level, Player player, BlockPos pos, boolean playEvent) {
-        if (level.isEmptyBlock(pos))
-            return false;
-
-        if (!level.isClientSide() && player instanceof ServerPlayer mplayer) {
-            pos = pos.immutable();
-
-            var state = level.getBlockState(pos);
-            var block = state.getBlock();
-
-            var event = new BlockEvent.BreakEvent(level, pos, state, mplayer);
-            if (MinecraftForge.EVENT_BUS.post(event))
-                return false;
-
-            if (playEvent) {
-                level.levelEvent(2001, pos, Block.getId(state));
-            }
-
-            boolean changed = level.setBlockAndUpdate(pos, state.getFluidState().createLegacyBlock());
-            if (changed) {
-                if (state.is(BlockTags.GUARDED_BY_PIGLINS)) {
-                    PiglinAi.angerNearbyPiglins(player, false);
-                }
-
-                if (!player.getAbilities().instabuild) {
-                    var tile = level.getBlockEntity(pos);
-
-                    block.destroy(level, pos, state);
-                    block.playerDestroy(level, player, pos, state, tile, stack);
-                    block.popExperience((ServerLevel) level, pos, event.getExpToDrop());
-                }
-
-                stack.mineBlock(level, state, pos, player);
-            }
-
-            mplayer.connection.send(new ClientboundBlockUpdatePacket(level, pos));
-
-            return true;
-        }
-
-        return false;
     }
 
     public static boolean harvestBlock(ItemStack stack, Level level, ServerPlayer player, BlockPos pos) {
