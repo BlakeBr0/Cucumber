a6bd20360a698058dfafe5a2f7fab0d558538960
BlakeBr0 <luasmrks@gmail.com>
2023-09-26 00:53:53 -0500

    Add mixin to fix compatibility issue with Kubejs

--- src/main/java/com/blakebr0/cucumber/helper/RecipeHelper.java
+++ src/main/java/com/blakebr0/cucumber/helper/RecipeHelper.java
@@ -1,5 +1,8 @@
 package com.blakebr0.cucumber.helper;
 
+import com.blakebr0.cucumber.Cucumber;
+import com.blakebr0.cucumber.event.RecipeManagerLoadingEvent;
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.Container;
@@ -7,12 +10,15 @@ import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.client.event.RecipesUpdatedEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public final class RecipeHelper {
     private static RecipeManager recipeManager;
@@ -52,5 +58,41 @@ public final class RecipeHelper {
 
         getRecipeManager().recipes.computeIfAbsent(recipe.getType(), t -> new HashMap<>()).put(recipe.getId(), recipe);
         getRecipeManager().byName.put(recipe.getId(), recipe);
+    }
+
+    public static void fireRecipeManagerLoadedEvent(RecipeManager manager, Map<RecipeType<?>, ImmutableMap.Builder<ResourceLocation, Recipe<?>>> map, ImmutableMap.Builder<ResourceLocation, Recipe<?>> builder) {
+        var stopwatch = Stopwatch.createStarted();
+        var recipes = new ArrayList<Recipe<?>>();
+
+        try {
+            MinecraftForge.EVENT_BUS.post(new RecipeManagerLoadingEvent(manager, recipes));
+        } catch (Exception e) {
+            Cucumber.LOGGER.error("An error occurred while firing RecipeManagerLoadingEvent", e);
+        }
+
+        for (var recipe : recipes) {
+            map.computeIfAbsent(recipe.getType(), t -> ImmutableMap.builder()).put(recipe.getId(), recipe);
+            builder.put(recipe.getId(), recipe);
+        }
+
+        Cucumber.LOGGER.info("Cucumber registered {} recipes in {} ms", recipes.size(), stopwatch.stop().elapsed(TimeUnit.MILLISECONDS));
+    }
+
+    public static void fireRecipeManagerLoadedEventKubeJSEdition(RecipeManager manager) {
+        var stopwatch = Stopwatch.createStarted();
+        var recipes = new ArrayList<Recipe<?>>();
+
+        try {
+            MinecraftForge.EVENT_BUS.post(new RecipeManagerLoadingEvent(manager, recipes));
+        } catch (Exception e) {
+            Cucumber.LOGGER.error("An error occurred while firing RecipeManagerLoadingEvent", e);
+        }
+
+        for (var recipe : recipes) {
+            manager.recipes.computeIfAbsent(recipe.getType(), t -> new HashMap<>()).put(recipe.getId(), recipe);
+            manager.byName.put(recipe.getId(), recipe);
+        }
+
+        Cucumber.LOGGER.info("Cucumber registered {} recipes in {} ms (KUBEJS MODE)", recipes.size(), stopwatch.stop().elapsed(TimeUnit.MILLISECONDS));
     }
 }
--- src/main/java/com/blakebr0/cucumber/mixin/RecipeManagerMixin.java
+++ src/main/java/com/blakebr0/cucumber/mixin/RecipeManagerMixin.java
@@ -1,8 +1,6 @@
 package com.blakebr0.cucumber.mixin;
 
-import com.blakebr0.cucumber.Cucumber;
-import com.blakebr0.cucumber.event.RecipeManagerLoadingEvent;
-import com.google.common.base.Stopwatch;
+import com.blakebr0.cucumber.helper.RecipeHelper;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonElement;
 import net.minecraft.resources.ResourceLocation;
@@ -11,16 +9,13 @@ import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeType;
-import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.ArrayList;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @Mixin(RecipeManager.class)
 public class RecipeManagerMixin {
@@ -44,21 +39,6 @@ public class RecipeManagerMixin {
             Map<RecipeType<?>, ImmutableMap.Builder<ResourceLocation, Recipe<?>>> map, // recipes
             ImmutableMap.Builder<ResourceLocation, Recipe<?>> builder // byName
     ) {
-        var stopwatch = Stopwatch.createStarted();
-        var manager = (RecipeManager) (Object) this;
-        var recipes = new ArrayList<Recipe<?>>();
-
-        try {
-            MinecraftForge.EVENT_BUS.post(new RecipeManagerLoadingEvent(manager, recipes));
-        } catch (Exception e) {
-            Cucumber.LOGGER.error("An error occurred while firing RecipeManagerLoadingEvent", e);
-        }
-
-        for (var recipe : recipes) {
-            map.computeIfAbsent(recipe.getType(), t -> ImmutableMap.builder()).put(recipe.getId(), recipe);
-            builder.put(recipe.getId(), recipe);
-        }
-
-        Cucumber.LOGGER.info("Cucumber registered {} recipes in {} ms", recipes.size(), stopwatch.stop().elapsed(TimeUnit.MILLISECONDS));
+        RecipeHelper.fireRecipeManagerLoadedEvent((RecipeManager) (Object) this, map, builder);
     }
 }
