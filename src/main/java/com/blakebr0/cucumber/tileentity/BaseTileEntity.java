509f164f7277743eb9b1d8020df7d56c0dd30c22
BlakeBr0 <luasmrks@gmail.com>
2023-10-28 19:29:13 -0500

    Add more granular methods to BaseTileEntity for marking/dispatching changed

--- src/main/java/com/blakebr0/cucumber/tileentity/BaseTileEntity.java
+++ src/main/java/com/blakebr0/cucumber/tileentity/BaseTileEntity.java
@@ -9,6 +9,8 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class BaseTileEntity extends BlockEntity {
+	private boolean isChanged = false;
+
 	public BaseTileEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
 		super(type, pos, state);
 	}
@@ -23,8 +25,33 @@ public class BaseTileEntity extends BlockEntity {
 		return this.saveWithFullMetadata();
 	}
 
+	@Deprecated(forRemoval = true)
 	public void markDirtyAndDispatch() {
+		this.setChangedAndDispatch();
+	}
+
+	public void setChanged() {
 		super.setChanged();
+		this.isChanged = true;
+	}
+
+	public void setChangedFast() {
+		if (this.level != null) {
+			this.level.blockEntityChanged(this.getBlockPos());
+			this.isChanged = true;
+		}
+	}
+
+	public void setChangedAndDispatch() {
+		this.setChanged();
 		TileEntityHelper.dispatchToNearbyPlayers(this);
+		this.isChanged = false;
+	}
+
+	public void dispatchIfChanged() {
+		if (this.isChanged) {
+			TileEntityHelper.dispatchToNearbyPlayers(this);
+			this.isChanged = false;
+		}
 	}
 }
