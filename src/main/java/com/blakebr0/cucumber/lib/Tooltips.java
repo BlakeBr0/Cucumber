c007568eb18a40a2da0c428962a64f510b3eb3d0
BlakeBr0 <luasmrks@gmail.com>
2023-11-18 23:01:53 -0600

    Add BaseWateringCanItem

--- src/main/java/com/blakebr0/cucumber/lib/Tooltips.java
+++ src/main/java/com/blakebr0/cucumber/lib/Tooltips.java
@@ -7,6 +7,8 @@ public final class Tooltips {
 	public static final Tooltip USES_LEFT = new Tooltip("tooltip.cucumber.uses_left");
 	public static final Tooltip ONE_USE_LEFT = new Tooltip("tooltip.cucumber.one_use_left");
 	public static final Tooltip UNLIMITED_USES = new Tooltip("tooltip.cucumber.unlimited_uses");
+	public static final Tooltip EMPTY = new Tooltip("tooltip.cucumber.empty");
+	public static final Tooltip FILLED = new Tooltip("tooltip.cucumber.filled");
 	public static final Tooltip HOLD_SHIFT_FOR_INFO = new Tooltip("tooltip.cucumber.hold_shift_for_info");
 	public static final Tooltip BLOCK_TAGS = new Tooltip("tooltip.cucumber.block_tags", ChatFormatting.DARK_GRAY);
 	public static final Tooltip ITEM_TAGS = new Tooltip("tooltip.cucumber.item_tags", ChatFormatting.DARK_GRAY);
