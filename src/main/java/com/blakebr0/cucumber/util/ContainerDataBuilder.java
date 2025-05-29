c97c450d8a09905a0ba7357726d84fc360bc1272
BlakeBr0 <luasmrks@gmail.com>
2025-05-29 17:11:59 -0500

    Add util class for defining ContainerData nicely

---
+++ src/main/java/com/blakebr0/cucumber/util/ContainerDataBuilder.java
@@ -0,0 +1,48 @@
+package com.blakebr0.cucumber.util;
+
+import net.minecraft.world.inventory.ContainerData;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntConsumer;
+import java.util.function.IntSupplier;
+
+public class ContainerDataBuilder {
+    private final List<DataSlotGetterSetter> data;
+
+    private ContainerDataBuilder() {
+        this.data = new ArrayList<>();
+    }
+
+    public static ContainerDataBuilder builder() {
+        return new ContainerDataBuilder();
+    }
+
+    public ContainerDataBuilder sync(IntSupplier getter, IntConsumer setter) {
+        this.data.add(new DataSlotGetterSetter(getter, setter));
+        return this;
+    }
+
+    public ContainerData build() {
+        return new ContainerData() {
+            private final List<DataSlotGetterSetter> data = ContainerDataBuilder.this.data;
+
+            @Override
+            public int get(int index) {
+                return this.data.get(index).getter.getAsInt();
+            }
+
+            @Override
+            public void set(int index, int value) {
+                this.data.get(index).setter.accept(value);
+            }
+
+            @Override
+            public int getCount() {
+                return this.data.size();
+            }
+        };
+    }
+
+    record DataSlotGetterSetter(IntSupplier getter, IntConsumer setter) { }
+}
