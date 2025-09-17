ebfc7f2a0a085126dd2bebf9d2111ac709a7d8b3
remakefactory <remakefactory5@gmail.com>
2025-09-17 21:56:11 +0800

    fluid tags

--- src/main/java/com/blakebr0/cucumber/client/handler/TagTooltipHandler.java
+++ src/main/java/com/blakebr0/cucumber/client/handler/TagTooltipHandler.java
@@ -1,11 +1,14 @@
 package com.blakebr0.cucumber.client.handler;
 
 import com.blakebr0.cucumber.config.ModConfigs;
+import com.blakebr0.cucumber.helper.FluidHelper;
 import com.blakebr0.cucumber.lib.Tooltips;
 import com.blakebr0.cucumber.util.Localizable;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -13,6 +16,7 @@ import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+import java.util.Comparator;
 import java.util.List;
 
 public final class TagTooltipHandler {
@@ -31,8 +35,13 @@ public final class TagTooltipHandler {
             var itemTags = stack.getTags()
                     .map(TagKey::location)
                     .toList();
+            var fluidTags = FluidHelper.getFluidTags(stack).values().stream()
+                    .flatMap(List::stream)
+                    .distinct()
+                    .sorted(Comparator.comparing(ResourceLocation::toString)) // 排序
+                    .toList();
 
-            if (!blockTags.isEmpty() || !itemTags.isEmpty()) {
+            if (!blockTags.isEmpty() || !itemTags.isEmpty() || !fluidTags.isEmpty()) {
                 var tooltip = event.getToolTip();
 
                 if (Screen.hasControlDown()) {
@@ -52,6 +61,13 @@ public final class TagTooltipHandler {
                                 .map(s -> "  " + s)
                                 .map(t -> Localizable.of(t).color(ChatFormatting.DARK_GRAY).build())
                                 .forEach(tooltip::add);
+                    }
+
+                    if (!fluidTags.isEmpty()) {
+                        tooltip.add(Tooltips.FLUID_TAGS.build());
+                        fluidTags.forEach(tag -> tooltip.add(
+                                Component.literal("  " + tag).withStyle(ChatFormatting.DARK_GRAY)
+                        ));
                     }
                 } else {
                     tooltip.add(Tooltips.HOLD_CTRL_FOR_TAGS.build());
--- src/main/java/com/blakebr0/cucumber/lib/Tooltips.java
+++ src/main/java/com/blakebr0/cucumber/lib/Tooltips.java
@@ -12,6 +12,7 @@ public final class Tooltips {
 	public static final Tooltip HOLD_SHIFT_FOR_INFO = new Tooltip("tooltip.cucumber.hold_shift_for_info");
 	public static final Tooltip BLOCK_TAGS = new Tooltip("tooltip.cucumber.block_tags", ChatFormatting.DARK_GRAY);
 	public static final Tooltip ITEM_TAGS = new Tooltip("tooltip.cucumber.item_tags", ChatFormatting.DARK_GRAY);
+	public static final Tooltip FLUID_TAGS = new Tooltip("tooltip.cucumber.fluid_tags", ChatFormatting.DARK_GRAY);
 	public static final Tooltip HOLD_CTRL_FOR_TAGS = new Tooltip("tooltip.cucumber.hold_ctrl_for_tags", ChatFormatting.DARK_GRAY);
 	public static final Tooltip HOLD_ALT_FOR_NBT = new Tooltip("tooltip.cucumber.hold_alt_for_nbt", ChatFormatting.DARK_GRAY);
 	public static final Tooltip FAILED_TO_LOAD_NBT = new Tooltip("tooltip.cucumber.failed_to_load_nbt", ChatFormatting.DARK_GRAY);
