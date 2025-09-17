f88f04120a9f8de0571a985fbef3a1cdbe4209a4
remakefactory <remakefactory5@gmail.com>
2025-09-17 19:16:26 +0800

    fluidTags

--- src/main/java/com/blakebr0/cucumber/lib/Tooltips.java
+++ src/main/java/com/blakebr0/cucumber/lib/Tooltips.java
@@ -12,6 +12,7 @@ public final class Tooltips {
 	public static final Tooltip HOLD_SHIFT_FOR_INFO = new Tooltip("tooltip.cucumber.hold_shift_for_info");
 	public static final Tooltip BLOCK_TAGS = new Tooltip("tooltip.cucumber.block_tags", ChatFormatting.DARK_GRAY);
 	public static final Tooltip ITEM_TAGS = new Tooltip("tooltip.cucumber.item_tags", ChatFormatting.DARK_GRAY);
+	public static final Tooltip FLUID_TAGS = new Tooltip("tooltip.cucumber.fluid_tags", ChatFormatting.DARK_GRAY);
 	public static final Tooltip HOLD_CTRL_FOR_TAGS = new Tooltip("tooltip.cucumber.hold_ctrl_for_tags", ChatFormatting.DARK_GRAY);
 	public static final Tooltip DATA_COMPONENTS = new Tooltip("tooltip.cucumber.data_components", ChatFormatting.DARK_GRAY);
 	public static final Tooltip HOLD_ALT_FOR_DATA_COMPONENTS = new Tooltip("tooltip.cucumber.hold_alt_for_data_components", ChatFormatting.DARK_GRAY);
