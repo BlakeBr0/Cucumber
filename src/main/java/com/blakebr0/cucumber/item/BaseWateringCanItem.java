187a445aca1df40d46e9758bd3297448b7259c9f
BlakeBr0 <luasmrks@gmail.com>
2025-10-09 23:11:32 -0500

    Pass immutable position when ticking blocks in BaseWateringCanItem
    closes #53

--- src/main/java/com/blakebr0/cucumber/item/BaseWateringCanItem.java
+++ src/main/java/com/blakebr0/cucumber/item/BaseWateringCanItem.java
@@ -211,7 +211,7 @@ public class BaseWateringCanItem extends BaseItem {
                     var plantBlock = state.getBlock();
 
                     if (plantBlock instanceof BonemealableBlock || plantBlock == Blocks.MYCELIUM || plantBlock == Blocks.CHORUS_FLOWER) {
-                        state.randomTick((ServerLevel) level, aoePos, random);
+                        state.randomTick((ServerLevel) level, aoePos.immutable(), random);
                     }
                 });
 
