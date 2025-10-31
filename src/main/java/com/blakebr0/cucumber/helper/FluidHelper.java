2f8a3c1256c4e2af8219d774b079cab6e0061631
remakefactory <remakefactory5@gmail.com>
2025-11-01 04:00:04 +0800

    fluid tags (#55)

--- src/main/java/com/blakebr0/cucumber/helper/FluidHelper.java
+++ src/main/java/com/blakebr0/cucumber/helper/FluidHelper.java
@@ -1,11 +1,20 @@
 package com.blakebr0.cucumber.helper;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.*;
+import java.util.stream.Stream;
 
 public final class FluidHelper {
 	public static FluidStack getFluidFromStack(ItemStack stack) {
@@ -39,5 +48,28 @@ public final class FluidHelper {
 
 	public static int toBuckets(int i) {
 		return i - (i % 1000);
+	}
+
+	public static Map<Fluid, List<ResourceLocation>> getFluidTags(ItemStack stack) {
+		return stack.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM).map(fluidHandler -> {
+			var fluidTagsMap = new LinkedHashMap<Fluid, List<ResourceLocation>>();
+			int tanks = fluidHandler.getTanks();
+
+			for (int i = 0; i < tanks; i++) {
+				var fluidStack = fluidHandler.getFluidInTank(i);
+				if (!fluidStack.isEmpty()) {
+					var fluid = fluidStack.getFluid();
+					fluidTagsMap.computeIfAbsent(fluid, f ->
+							ForgeRegistries.FLUIDS.getHolder(f)
+									.map(Holder::tags)
+									.orElse(Stream.empty())
+									.map(TagKey::location)
+									.sorted(Comparator.comparing(ResourceLocation::toString))
+									.toList()
+					);
+				}
+			}
+			return fluidTagsMap;
+		}).orElse(new LinkedHashMap<>());
 	}
 }
