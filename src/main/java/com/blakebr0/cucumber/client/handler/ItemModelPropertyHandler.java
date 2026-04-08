eda3f6abc477beb4388ec583e75f0791fb91c6c1
BlakeBr0 <luasmrks@gmail.com>
2026-04-08 16:55:04 -0500

    Register CustomCrossbowPullProperty

--- src/main/java/com/blakebr0/cucumber/client/handler/ItemModelPropertyHandler.java
+++ src/main/java/com/blakebr0/cucumber/client/handler/ItemModelPropertyHandler.java
@@ -2,6 +2,7 @@ package com.blakebr0.cucumber.client.handler;
 
 import com.blakebr0.cucumber.Cucumber;
 import com.blakebr0.cucumber.client.properties.CustomBowPullProperty;
+import com.blakebr0.cucumber.client.properties.CustomCrossbowPullProperty;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.RegisterRangeSelectItemModelPropertyEvent;
 
@@ -9,5 +10,6 @@ public final class ItemModelPropertyHandler {
     @SubscribeEvent
     public void onRegisterRangeSelectItemModelProperties(RegisterRangeSelectItemModelPropertyEvent event) {
         event.register(Cucumber.resource("custom_bow_pull"), CustomBowPullProperty.MAP_CODEC);
+        event.register(Cucumber.resource("custom_crossbow_pull"), CustomCrossbowPullProperty.MAP_CODEC);
     }
 }
