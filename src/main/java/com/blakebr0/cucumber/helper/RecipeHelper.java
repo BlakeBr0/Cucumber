a89f8b0f5d5f9b2c2ca30045158b7ebec2812f7f
BlakeBr0 <luasmrks@gmail.com>
2026-04-15 16:21:24 -0500

    Remove unused functions from RecipeHelper

--- src/main/java/com/blakebr0/cucumber/helper/RecipeHelper.java
+++ src/main/java/com/blakebr0/cucumber/helper/RecipeHelper.java
@@ -4,28 +4,16 @@ import com.blakebr0.cucumber.Cucumber;
 import com.blakebr0.cucumber.event.RecipeManagerLoadedEvent;
 import com.blakebr0.cucumber.event.RecipeManagerLoadingEvent;
 import com.google.common.base.Stopwatch;
-import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
-import net.minecraft.world.item.crafting.RecipeInput;
 import net.minecraft.world.item.crafting.RecipeManager;
-import net.minecraft.world.item.crafting.RecipeType;
 import net.neoforged.neoforge.common.NeoForge;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public final class RecipeHelper {
-    public static <I extends RecipeInput, T extends Recipe<I>> Collection<RecipeHolder<T>> byType(RecipeManager manager, RecipeType<T> type) {
-        return manager.recipeMap().byType(type);
-    }
-
-    public static <I extends RecipeInput, T extends Recipe<I>> List<T> byTypeValues(RecipeManager manager, RecipeType<T> type) {
-        return byType(manager, type).stream().map(RecipeHolder::value).toList();
-    }
-
     @ApiStatus.Internal
     public static void fireRecipeManagerLoadingEvent(RecipeManager manager, List<RecipeHolder<?>> output) {
         var stopwatch = Stopwatch.createStarted();
