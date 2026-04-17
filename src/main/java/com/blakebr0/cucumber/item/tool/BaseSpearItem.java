0a2985ed13a2ce73252617db3ee540297165eba6
BlakeBr0 <luasmrks@gmail.com>
2026-04-16 20:24:45 -0500

    Add overload that provides properties for BaseSpearItem

--- src/main/java/com/blakebr0/cucumber/item/tool/BaseSpearItem.java
+++ src/main/java/com/blakebr0/cucumber/item/tool/BaseSpearItem.java
@@ -4,6 +4,8 @@ import com.blakebr0.cucumber.item.BaseItem;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.ToolMaterial;
 
+import java.util.function.Function;
+
 public class BaseSpearItem extends BaseItem {
     public BaseSpearItem(
             Identifier id,
@@ -19,5 +21,22 @@ public class BaseSpearItem extends BaseItem {
             float damageThreshold
     ) {
         super(id, p -> p.spear(material, attackDuration, damageMultiplier, delay, dismountTime, dismountThreshold, knockbackTime, knockbackThreshold, damageTime, damageThreshold));
+    }
+
+    public BaseSpearItem(
+            Identifier id,
+            ToolMaterial material,
+            float attackDuration,
+            float damageMultiplier,
+            float delay,
+            float dismountTime,
+            float dismountThreshold,
+            float knockbackTime,
+            float knockbackThreshold,
+            float damageTime,
+            float damageThreshold,
+            Function<Properties, Properties> properties
+    ) {
+        super(id, properties.compose(p -> p.spear(material, attackDuration, damageMultiplier, delay, dismountTime, dismountThreshold, knockbackTime, knockbackThreshold, damageTime, damageThreshold)));
     }
 }
