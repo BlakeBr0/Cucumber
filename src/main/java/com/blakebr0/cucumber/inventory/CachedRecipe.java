b2d7cb8275e17b980e44041dae5ead4df14fa24b
BlakeBr0 <luasmrks@gmail.com>
2026-04-23 16:15:13 -0500

    Use RecipeHolder in CachedRecipe and expose recipe id

--- src/main/java/com/blakebr0/cucumber/inventory/CachedRecipe.java
+++ src/main/java/com/blakebr0/cucumber/inventory/CachedRecipe.java
@@ -1,15 +1,17 @@
 package com.blakebr0.cucumber.inventory;
 
+import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.RecipeInput;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
+import org.jspecify.annotations.Nullable;
 
 public class CachedRecipe<I extends RecipeInput, T extends Recipe<I>> {
     private final RecipeType<T> type;
-    private T recipe;
+    private @Nullable RecipeHolder<T> recipe;
 
     public CachedRecipe(RecipeType<T> type) {
         this.type = type;
@@ -17,17 +19,17 @@ public class CachedRecipe<I extends RecipeInput, T extends Recipe<I>> {
 
     /**
      * Checks if the provided inventory matches the cached recipe. If it doesn't match, it will try to find a new recipe
+     *
      * @param inventory the recipe inventory
-     * @param level the level
+     * @param level     the level
      * @return does this recipe match the inventory?
      */
     public boolean check(I inventory, ServerLevel level) {
-        if (this.recipe != null && this.recipe.matches(inventory, level))
+        if (this.recipe != null && this.recipe.value().matches(inventory, level))
             return true;
 
         this.recipe = level.recipeAccess()
                 .getRecipeFor(this.type, inventory, level)
-                .map(RecipeHolder::value)
                 .orElse(null);
 
         return this.recipe != null;
@@ -45,12 +47,17 @@ public class CachedRecipe<I extends RecipeInput, T extends Recipe<I>> {
     }
 
     public T get() {
-        return this.recipe;
+        return this.recipe != null ? this.recipe.value() : null;
+    }
+
+    public Identifier id() {
+        return this.recipe != null ? this.recipe.id().identifier() : null;
     }
 
     public T checkAndGet(I inventory, ServerLevel level) {
-        if (this.check(inventory, level))
-            return this.recipe;
+        if (this.check(inventory, level)) {
+            return this.get();
+        }
 
         return null;
     }
