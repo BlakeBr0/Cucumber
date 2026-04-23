d4654506bcb6997d9e9c1126d3b1278925fc658a
BlakeBr0 <luasmrks@gmail.com>
2026-04-22 22:00:43 -0500

    Change ItemResourceHelper to use CItemstacksHandler insert with bypass

--- src/main/java/com/blakebr0/cucumber/helper/ItemResourceHelper.java
+++ src/main/java/com/blakebr0/cucumber/helper/ItemResourceHelper.java
@@ -1,14 +1,14 @@
 package com.blakebr0.cucumber.helper;
 
+import com.blakebr0.cucumber.inventory.CItemStacksHandler;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.transfer.item.ItemResource;
-import net.neoforged.neoforge.transfer.item.ItemStacksResourceHandler;
 import net.neoforged.neoforge.transfer.transaction.Transaction;
 
 public final class ItemResourceHelper {
-    public static boolean canCombine(ItemStacksResourceHandler handler, int slot, ItemStack stack) {
+    public static boolean canCombine(CItemStacksHandler handler, int slot, ItemStack stack) {
         try (var tx = Transaction.openRoot()) {
-            return handler.insert(slot, ItemResource.of(stack), stack.count(), tx) == stack.count();
+            return handler.insert(slot, ItemResource.of(stack), stack.count(), tx, true) == stack.count();
         }
     }
 }
