06233f5486dd5cf4b079ed8f1a06371d131f9676
Clem76 <39047200+Daudeuf@users.noreply.github.com>
2026-05-04 04:02:21 +0200

    Implement Clearable interface to some blocks to made it work with Sable (#56)

--- src/main/java/com/blakebr0/cucumber/tileentity/BaseInventoryTileEntity.java
+++ src/main/java/com/blakebr0/cucumber/tileentity/BaseInventoryTileEntity.java
@@ -4,11 +4,12 @@ import com.blakebr0.cucumber.inventory.BaseItemStackHandler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.Clearable;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
-public abstract class BaseInventoryTileEntity extends BaseTileEntity {
+public abstract class BaseInventoryTileEntity extends BaseTileEntity implements Clearable {
     public BaseInventoryTileEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
@@ -29,5 +30,10 @@ public abstract class BaseInventoryTileEntity extends BaseTileEntity {
     public boolean isUsableByPlayer(Player player) {
         var pos = this.getBlockPos();
         return player.distanceToSqr(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5) <= 64;
+    }
+
+    @Override
+    public void clearContent() {
+        this.getInventory().getStacks().clear();
     }
 }
