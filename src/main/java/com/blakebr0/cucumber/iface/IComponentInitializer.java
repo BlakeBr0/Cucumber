dc7375980d5543efda410f49f09e671c968ad469
BlakeBr0 <luasmrks@gmail.com>
2026-05-03 22:14:02 -0500

    Add mixin to re-add method to modify data components on ItemStack instantiation

---
+++ src/main/java/com/blakebr0/cucumber/iface/IComponentInitializer.java
@@ -0,0 +1,7 @@
+package com.blakebr0.cucumber.iface;
+
+import net.minecraft.world.item.ItemStack;
+
+public interface IComponentInitializer {
+    void initialize(ItemStack stack);
+}
--- src/main/java/com/blakebr0/cucumber/mixin/ItemStackMixin.java
+++ src/main/java/com/blakebr0/cucumber/mixin/ItemStackMixin.java
@@ -1,6 +1,9 @@
 package com.blakebr0.cucumber.mixin;
 
 import com.blakebr0.cucumber.event.ItemBreakEvent;
+import com.blakebr0.cucumber.iface.IComponentInitializer;
+import net.minecraft.core.Holder;
+import net.minecraft.core.component.PatchedDataComponentMap;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -16,7 +19,21 @@ import java.util.function.Consumer;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
-    @Shadow public abstract ItemStack copy();
+    @Shadow
+    public abstract ItemStack copy();
+
+    @Shadow
+    public abstract Item getItem();
+
+    @Inject(
+            at = @At("RETURN"),
+            method = "<init>(Lnet/minecraft/core/Holder;ILnet/minecraft/core/component/PatchedDataComponentMap;)V"
+    )
+    public void cucumber$constructor(Holder<Item> item, int count, PatchedDataComponentMap components, CallbackInfo ci) {
+        if (this.getItem() instanceof IComponentInitializer initializer) {
+            initializer.initialize((ItemStack) (Object) this);
+        }
+    }
 
     @Inject(
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;shrink(I)V", ordinal = 0),
