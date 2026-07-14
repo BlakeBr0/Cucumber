f5d13bb3f509f9e4b637ceb1330a4ece8e819090
BlakeBr0 <luasmrks@gmail.com>
2026-07-14 15:54:12 -0500

    Redirect more public resource handler methods in SidedInventoryWrapper

--- src/main/java/com/blakebr0/cucumber/inventory/SidedInventoryWrapper.java
+++ src/main/java/com/blakebr0/cucumber/inventory/SidedInventoryWrapper.java
@@ -1,12 +1,18 @@
 package com.blakebr0.cucumber.inventory;
 
 import net.minecraft.core.Direction;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
+import net.neoforged.neoforge.transfer.TransferPreconditions;
 import net.neoforged.neoforge.transfer.item.ItemResource;
 import net.neoforged.neoforge.transfer.item.ItemStacksResourceHandler;
 import net.neoforged.neoforge.transfer.transaction.TransactionContext;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Objects;
 
 public class SidedInventoryWrapper extends ItemStacksResourceHandler {
     private final CItemStacksHandler inventory;
@@ -39,8 +45,50 @@ public class SidedInventoryWrapper extends ItemStacksResourceHandler {
     }
 
     @Override
+    public void set(int index, ItemResource resource, int amount) {
+        TransferPreconditions.checkNonNegative(amount);
+        if (resource.isEmpty() && amount > 0) {
+            throw new IllegalArgumentException("Resource is empty but the amount is positive: " + amount);
+        }
+
+        var oldContents = this.inventory.getStacks().set(index, getStackFrom(resource, amount));
+        onContentsChanged(index, oldContents);
+    }
+
+    @Override
+    public int size() {
+        return this.inventory.size();
+    }
+
+    @Override
     public ItemResource getResource(int index) {
         return this.inventory.getResource(index);
+    }
+
+    @Override
+    public long getAmountAsLong(int index) {
+        Objects.checkIndex(index, size());
+        return getAmountFrom(this.inventory.getStacks().get(index));
+    }
+
+    @Override
+    public boolean isValid(int index, ItemResource resource) {
+        return this.inventory.isValid(index, resource);
+    }
+
+    @Override
+    public void serialize(ValueOutput output) {
+        this.inventory.serialize(output);
+    }
+
+    @Override
+    public void deserialize(ValueInput input) {
+        this.inventory.deserialize(input);
+    }
+
+    @Override
+    public NonNullList<ItemStack> copyToList() {
+        return NonNullList.of(this.emptyStack, this.inventory.getStacks().toArray(new ItemStack[0]));
     }
 
     public int getSlotLimit(int slot) {
